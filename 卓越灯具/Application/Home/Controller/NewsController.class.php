8b5d45a8656e1f4f6ef48c848d4b3759d9d0b3a5
zhangzhi2016 <2767462109@qq.com>
2017-05-25 14:34:24 +0800

    controller

---
+++ 卓越灯具/Application/Home/Controller/NewsController.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class NewsController extends Controller {
+    public function news(){
+        $this->display();
+    }
+}
---
+++ 卓越灯具/Application/Home/Controller/ShowController.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class ShowController extends Controller {
+    public function show(){
+        $this->display();
+    }
+}

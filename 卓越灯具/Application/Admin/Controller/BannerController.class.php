c3e1015be7aca840733d81f4b5a9b3d72f2bec07
zhaoboyan <996433896@qq.com>
2017-05-25 10:41:20 +0800

    Delete BannerController.class.php

--- 卓越灯具/Application/Admin/Controller/BannerController.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-	namespace Admin\Controller;
-	use Think\Controller;
-	class BannerController extends Controller {
-         public function banner(){
-         	$this->display();
-         }
-     }

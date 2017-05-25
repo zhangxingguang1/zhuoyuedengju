11b9e6f9f7ee4cca4211c78cec47ebb4f73184b8
zby <zby>
2017-05-25 10:29:43 +0800

    后台控制器

---
+++ 卓越灯具/Application/Admin/Controller/ClassifyController.class.php
@@ -0,0 +1,8 @@
+<?php
+	namespace Admin\Controller;
+	use Think\Controller;
+	class ClassifyController extends Controller {
+         public function Classify(){
+         	$this->display();
+         }
+     }

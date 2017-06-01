90630221ee96e53d480b2cd62aa627558c32a958
zhangzhi2016 <2767462109@qq.com>
2017-06-01 08:16:30 +0800

    home

---
+++ 卓越灯具/Application/Home/Controller/IntroController.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class IntroController extends Controller {
+    public function intro(){
+        $this->display();
+    }
+}
---
+++ 卓越灯具/Application/Home/Controller/ProductController.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class ProductController extends Controller {
+    public function product(){
+        $this->display();
+    }
+}

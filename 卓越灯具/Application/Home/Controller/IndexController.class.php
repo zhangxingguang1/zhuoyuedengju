0161a2e30baddf9010e80a1794152305eaf9369c
zhangxingguang1 <991067295@qq.com>
2017-06-05 16:41:03 +0800

    修改前台首页控制器

--- 卓越灯具/Application/Home/Controller/IndexController.class.php
+++ 卓越灯具/Application/Home/Controller/IndexController.class.php
@@ -5,4 +5,29 @@ class IndexController extends Controller {
     public function index(){
         $this->display();
     }
+	public function ajax(){
+     	$data=array();
+     	$data['content']=$_POST['content'];
+        $data['goid']=$_POST['goid'];
+        $data['toid']=$_POST['toid'];
+        $data['times']=$_POST['time'];
+     	$dd=M('msg')->add($data);
+	}
+	public function fresh(){
+        $pid=$_SESSION['name'];
+		$data=M('msg')->where("toid=$pid")->order('id desc')->limit('20')->select();
+		$this->ajaxReturn($data);
+	}
+    public function chedan(){
+        $data=M('session')->where("states=0")->order('id asc')->limit('1')->select();
+        $this->ajaxReturn($data);
+    }
+    public function chedand(){
+        $pid=$_POST['toid'];
+        $_SESSION['name']=$pid;
+        $goods=M('session');
+        $goods->states=1;
+        $goods->state=0;
+        $goods->where("id=$pid")->save();
+    }
 }

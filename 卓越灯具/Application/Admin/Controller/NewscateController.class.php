e20e966e4316d53f089e2f9f092885bcfb4c0697
zhangjiawei0 <15231120650@sina.cn>
2017-06-05 16:28:36 +0800

    新闻分类控制器

---
+++ 卓越灯具/Application/Admin/Controller/NewscateController.class.php
@@ -0,0 +1,11 @@
+<?php
+	namespace Admin\Controller;
+	use Think\Controller;
+	class NewscateController extends Controller {
+         public function newscate(){
+         	$this->display();
+         }
+         public function newscate_add(){
+         	$this->display();
+         }
+     }

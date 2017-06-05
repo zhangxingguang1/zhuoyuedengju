e7f27f549d04bed951d97ed5aff1158f3b510c04
zhangxingguang1 <991067295@qq.com>
2017-06-05 16:38:32 +0800

    修改后台客服控制器

--- 卓越灯具/Application/Admin/Controller/NownewsController.class.php
+++ 卓越灯具/Application/Admin/Controller/NownewsController.class.php
@@ -3,6 +3,22 @@
 	use Think\Controller;
 	class NownewsController extends Controller {
         public  function nownews(){
+        	$goods=M('session');
+            $good=$goods->where("state=1")->order('id asc')->select();
+            $this->assign("reli",$good);
          	$this->display();
-         }
+        }
+        public function freshes(){
+	        $data=M('msg')->where("goid=0")->order('id desc')->limit('60')->select();
+	        $this->ajaxReturn($data);
+	    }
+        public function sear(){
+        $data=M('session')->where("states=1&&state=0")->order('id asc')->limit('1')->select();
+        $pid=$data[0]['id'];
+        $goods=M('session');
+        $goods->state=1;
+        $goods->times=time();
+        $goods->where("id=$pid")->save();
+        $this->ajaxReturn($data);
+    }
      }
